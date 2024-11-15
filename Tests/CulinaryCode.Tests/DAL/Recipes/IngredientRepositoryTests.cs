66a2eff01447302b2d969d5d5592d81556a8e94a
steffen-vochten <steffen.vochten@infosupport.com>
2024-11-15 15:36:53 +0100

    add repository tests

---
+++ Tests/CulinaryCode.Tests/DAL/Recipes/IngredientRepositoryTests.cs
@@ -0,0 +1,169 @@
+﻿using DAL.EF;
+using DAL.Recipes;
+using DOM.Exceptions;
+using DOM.Recipes.Ingredients;
+using Microsoft.EntityFrameworkCore;
+
+namespace CulinaryCode.Tests.DAL.Recipes;
+
+public class IngredientRepositoryTests
+{
+    private readonly CulinaryCodeDbContext _dbContext;
+    private readonly IIngredientRepository _ingredientRepository;
+    
+    public IngredientRepositoryTests()
+    {
+        var options = new DbContextOptionsBuilder<CulinaryCodeDbContext>()
+            // force unique database for each test so data is isolated
+            .UseInMemoryDatabase(databaseName: Guid.NewGuid().ToString()) 
+            .Options;
+        
+        _dbContext = new CulinaryCodeDbContext(options);
+        _ingredientRepository = new IngredientRepository(_dbContext);
+    }
+    
+    [Fact]
+    public void ReadIngredientById_IngredientExists_ReturnsIngredient()
+    {
+        // Arrange
+        var ingredient = new Ingredient
+        {
+            IngredientId = Guid.NewGuid(),
+            IngredientName = "Test Ingredient",
+            Measurement = MeasurementType.Gram
+        };
+        _dbContext.Ingredients.Add(ingredient);
+        _dbContext.SaveChanges();
+        
+        // Act
+        var result = _ingredientRepository.ReadIngredientById(ingredient.IngredientId);
+        
+        // Assert
+        Assert.NotNull(result);
+        Assert.Equal(ingredient.IngredientId, result.IngredientId);
+        Assert.Equal(ingredient.IngredientName, result.IngredientName);
+    }
+    
+    [Fact]
+    public void ReadIngredientById_IngredientDoesNotExist_ThrowsIngredientNotFoundException()
+    {
+        // Arrange
+        var ingredientId = Guid.NewGuid();
+        
+        // Act & Assert
+        Assert.Throws<IngredientNotFoundException>(() => _ingredientRepository.ReadIngredientById(ingredientId));
+    }
+    
+    [Fact]
+    public void ReadIngredientByName_IngredientExists_ReturnsIngredient()
+    {
+        // Arrange
+        var ingredient = new Ingredient
+        {
+            IngredientId = Guid.NewGuid(),
+            IngredientName = "Test Ingredient",
+            Measurement = MeasurementType.Gram
+        };
+        _dbContext.Ingredients.Add(ingredient);
+        _dbContext.SaveChanges();
+        
+        // Act
+        var result = _ingredientRepository.ReadIngredientByName(ingredient.IngredientName);
+        
+        // Assert
+        Assert.NotNull(result);
+        Assert.Equal(ingredient.IngredientId, result.IngredientId);
+        Assert.Equal(ingredient.IngredientName, result.IngredientName);
+    }
+    
+    [Fact]
+    public void ReadIngredientByName_IngredientDoesNotExist_ThrowsIngredientNotFoundException()
+    {
+        // Arrange
+        var ingredientName = "Test Ingredient";
+        
+        // Act & Assert
+        Assert.Throws<IngredientNotFoundException>(() => _ingredientRepository.ReadIngredientByName(ingredientName));
+    }
+    
+    [Fact]
+    public void ReadIngredientByNameAndMeasurementType_IngredientExists_ReturnsIngredient()
+    {
+        // Arrange
+        var ingredient = new Ingredient
+        {
+            IngredientId = Guid.NewGuid(),
+            IngredientName = "Test Ingredient",
+            Measurement = MeasurementType.Gram
+        };
+        _dbContext.Ingredients.Add(ingredient);
+        _dbContext.SaveChanges();
+        
+        // Act
+        var result = _ingredientRepository.ReadIngredientByNameAndMeasurementType(ingredient.IngredientName, ingredient.Measurement);
+        
+        // Assert
+        Assert.NotNull(result);
+        Assert.Equal(ingredient.IngredientId, result.IngredientId);
+        Assert.Equal(ingredient.IngredientName, result.IngredientName);
+        Assert.Equal(ingredient.Measurement, result.Measurement);
+    }
+    
+    [Fact]
+    public void ReadIngredientByNameAndMeasurementType_IngredientDoesNotExist_ThrowsIngredientNotFoundException()
+    {
+        // Arrange
+        var ingredientName = "Test Ingredient";
+        var measurementType = MeasurementType.Gram;
+        
+        // Act & Assert
+        Assert.Throws<IngredientNotFoundException>(() => _ingredientRepository.ReadIngredientByNameAndMeasurementType(ingredientName, measurementType));
+    }
+    
+    [Fact]
+    public void CreateIngredient_IngredientDoesNotExist_CreatesIngredient()
+    {
+        // Arrange
+        var ingredient = new Ingredient
+        {
+            IngredientId = Guid.NewGuid(),
+            IngredientName = "Test Ingredient",
+            Measurement = MeasurementType.Gram
+        };
+        
+        // Act
+        _ingredientRepository.CreateIngredient(ingredient);
+        
+        // Assert
+        var result = _dbContext.Ingredients.Find(ingredient.IngredientId);
+        Assert.NotNull(result);
+        Assert.Equal(ingredient.IngredientId, result.IngredientId);
+        Assert.Equal(ingredient.IngredientName, result.IngredientName);
+        Assert.Equal(ingredient.Measurement, result.Measurement);
+    }
+    
+    [Fact]
+    public void UpdateIngredient_IngredientExists_UpdatesIngredient()
+    {
+        // Arrange
+        var ingredient = new Ingredient
+        {
+            IngredientId = Guid.NewGuid(),
+            IngredientName = "Test Ingredient",
+            Measurement = MeasurementType.Gram
+        };
+        _dbContext.Ingredients.Add(ingredient);
+        _dbContext.SaveChanges();
+        
+        // Act
+        ingredient.IngredientName = "Updated Ingredient";
+        _ingredientRepository.UpdateIngredient(ingredient);
+        
+        // Assert
+        var result = _dbContext.Ingredients.Find(ingredient.IngredientId);
+        Assert.NotNull(result);
+        Assert.Equal(ingredient.IngredientId, result.IngredientId);
+        Assert.Equal(ingredient.IngredientName, result.IngredientName);
+        Assert.Equal(ingredient.Measurement, result.Measurement);
+    }
+}
---
+++ Tests/CulinaryCode.Tests/DAL/Recipes/RecipeRepositoryTests.cs
@@ -0,0 +1,191 @@
+﻿using DAL.EF;
+using DAL.Recipes;
+using DOM.Accounts;
+using DOM.Exceptions;
+using DOM.Recipes;
+using DOM.Recipes.Ingredients;
+using Microsoft.EntityFrameworkCore;
+
+namespace CulinaryCode.Tests.DAL.Recipes;
+
+public class RecipeRepositoryTests
+{
+    private readonly CulinaryCodeDbContext _dbContext;
+    private readonly IRecipeRepository _recipeRepository;
+    
+    public RecipeRepositoryTests()
+    {
+        var options = new DbContextOptionsBuilder<CulinaryCodeDbContext>()
+            // force unique database for each test so data is isolated
+            .UseInMemoryDatabase(databaseName: Guid.NewGuid().ToString()) 
+            .Options;
+        
+        _dbContext = new CulinaryCodeDbContext(options);
+        _recipeRepository = new RecipeRepository(_dbContext);
+    }
+    
+    [Fact]
+    public void ReadRecipeById_RecipeExists_ReturnsRecipe()
+    {
+        // Arrange
+        var recipe = new Recipe
+        {
+            RecipeId = Guid.NewGuid(),
+            RecipeName = "Test Recipe",
+            Ingredients = new List<IngredientQuantity>(),
+            Instructions = new List<InstructionStep>(),
+            Reviews = new List<Review>(),
+            Preferences = new List<Preference>()
+        };
+        _dbContext.Recipes.Add(recipe);
+        _dbContext.SaveChanges();
+        
+        // Act
+        var result = _recipeRepository.ReadRecipeById(recipe.RecipeId);
+        
+        // Assert
+        Assert.NotNull(result);
+        Assert.Equal(recipe.RecipeId, result.RecipeId);
+        Assert.Equal(recipe.RecipeName, result.RecipeName);
+    }
+    
+    [Fact]
+    public void ReadRecipeById_RecipeDoesNotExist_ThrowsRecipeNotFoundException()
+    {
+        // Arrange
+        var recipeId = Guid.NewGuid();
+        
+        // Act & Assert
+        Assert.Throws<RecipeNotFoundException>(() => _recipeRepository.ReadRecipeById(recipeId));
+    }
+    
+    [Fact]
+    public void ReadRecipeByName_RecipeExists_ReturnsRecipe()
+    {
+        // Arrange
+        var recipe = new Recipe
+        {
+            RecipeId = Guid.NewGuid(),
+            RecipeName = "Test Recipe",
+            Ingredients = new List<IngredientQuantity>(),
+            Instructions = new List<InstructionStep>(),
+            Reviews = new List<Review>(),
+            Preferences = new List<Preference>()
+        };
+        _dbContext.Recipes.Add(recipe);
+        _dbContext.SaveChanges();
+        
+        // Act
+        var result = _recipeRepository.ReadRecipeByName(recipe.RecipeName);
+        
+        // Assert
+        Assert.NotNull(result);
+        Assert.Equal(recipe.RecipeId, result.RecipeId);
+        Assert.Equal(recipe.RecipeName, result.RecipeName);
+    }
+    
+    [Fact]
+    public void ReadRecipeByName_RecipeDoesNotExist_ThrowsRecipeNotFoundException()
+    {
+        // Arrange
+        var recipeName = "Test Recipe";
+        
+        // Act & Assert
+        Assert.Throws<RecipeNotFoundException>(() => _recipeRepository.ReadRecipeByName(recipeName));
+    }
+    
+    [Fact]
+    public void ReadRecipesCollectionByName_RecipesExist_ReturnsCollection()
+    {
+        // Arrange
+        var recipe1 = new Recipe
+        {
+            RecipeId = Guid.NewGuid(),
+            RecipeName = "Test Recipe 1",
+            Ingredients = new List<IngredientQuantity>(),
+            Instructions = new List<InstructionStep>(),
+            Reviews = new List<Review>(),
+            Preferences = new List<Preference>()
+        };
+        var recipe2 = new Recipe
+        {
+            RecipeId = Guid.NewGuid(),
+            RecipeName = "Test Recipe 2",
+            Ingredients = new List<IngredientQuantity>(),
+            Instructions = new List<InstructionStep>(),
+            Reviews = new List<Review>(),
+            Preferences = new List<Preference>()
+        };
+        _dbContext.Recipes.Add(recipe1);
+        _dbContext.Recipes.Add(recipe2);
+        _dbContext.SaveChanges();
+        
+        // Act
+        var result = _recipeRepository.ReadRecipesCollectionByName("Test Recipe");
+        
+        // Assert
+        Assert.NotNull(result);
+        Assert.Equal(2, result.Count);
+    }
+    
+    [Fact]
+    public void ReadRecipesCollectionByName_RecipesDoNotExist_ThrowsRecipeNotFoundException()
+    {
+        // Arrange
+        var recipeName = "Test Recipe";
+        
+        // Act & Assert
+        Assert.Throws<RecipeNotFoundException>(() => _recipeRepository.ReadRecipesCollectionByName(recipeName));
+    }
+    
+    [Fact]
+    public void CreateRecipe_ValidRecipe_CreatesRecipe()
+    {
+        // Arrange
+        var recipe = new Recipe
+        {
+            RecipeId = Guid.NewGuid(),
+            RecipeName = "Test Recipe",
+            Ingredients = new List<IngredientQuantity>(),
+            Instructions = new List<InstructionStep>(),
+            Reviews = new List<Review>(),
+            Preferences = new List<Preference>()
+        };
+        
+        // Act
+        _recipeRepository.CreateRecipe(recipe);
+        
+        // Assert
+        var result = _dbContext.Recipes.Find(recipe.RecipeId);
+        Assert.NotNull(result);
+        Assert.Equal(recipe.RecipeId, result.RecipeId);
+        Assert.Equal(recipe.RecipeName, result.RecipeName);
+    }
+    
+    [Fact]
+    public void UpdateRecipe_ValidRecipe_UpdatesRecipe()
+    {
+        // Arrange
+        var recipe = new Recipe
+        {
+            RecipeId = Guid.NewGuid(),
+            RecipeName = "Test Recipe",
+            Ingredients = new List<IngredientQuantity>(),
+            Instructions = new List<InstructionStep>(),
+            Reviews = new List<Review>(),
+            Preferences = new List<Preference>()
+        };
+        _dbContext.Recipes.Add(recipe);
+        _dbContext.SaveChanges();
+        
+        // Act
+        recipe.RecipeName = "Updated Recipe";
+        _recipeRepository.UpdateRecipe(recipe);
+        
+        // Assert
+        var result = _dbContext.Recipes.Find(recipe.RecipeId);
+        Assert.NotNull(result);
+        Assert.Equal(recipe.RecipeId, result.RecipeId);
+        Assert.Equal(recipe.RecipeName, result.RecipeName);
+    }
+}
