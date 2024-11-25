89d7aa30ebd49c4e1fc9094a4e9e35e4e2581457
steffen-vochten <steffen.vochten@infosupport.com>
2024-11-25 18:23:38 +0100

    add simple controller to check connection

---
+++ WEBAPI/Controllers/ConnectionController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WEBAPI.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class ConnectionController : ControllerBase
+{
+    private readonly ILogger<ConnectionController> _logger;
+
+    public ConnectionController(ILogger<ConnectionController> logger)
+    {
+        _logger = logger;
+    }
+    
+    [HttpGet]
+    public Task<bool> Get()
+    {
+        return Task.FromResult(true);
+    }
+}
