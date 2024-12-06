84e704179c7f8f8479f6c2d14a3039c718352bad
HannesC <hannes.coenen@infosupport.com>
2024-12-06 14:42:26 +0100

    database job changed

--- BL/Scheduled/RefreshRecipeDatabaseJob.cs
+++ BL/Scheduled/RefreshRecipeDatabaseJob.cs
@@ -28,14 +28,30 @@ public class RefreshRecipeDatabaseJob : IJob
         
         int minAmountInDatabase = _jobSettingsOptions.MinAmount;
 
-        await _recipeManager.RemoveUnusedRecipes();
+        var removeUnusedRecipesResult = await _recipeManager.RemoveUnusedRecipes();
+        if (!removeUnusedRecipesResult.IsSuccess)
+        {
+            _logger.LogError($"Failed to remove unused recipes");
+            return;
+        }
         
         // Count amount of recipes
-        var count = await _recipeManager.GetAmountOfRecipes();
+        var countResult = await _recipeManager.GetAmountOfRecipes();
+        if (!countResult.IsSuccess)
+        {
+            _logger.LogError($"DatabaseJob failed to get amount from database: {countResult.ErrorMessage}");
+            return;
+        }
+        var count = countResult.Value;
 
         var amountToCreate = minAmountInDatabase - count;
         
-        await _recipeManager.CreateBatchRandomRecipes(amountToCreate, null);
+        var createBatchRecipeResult = await _recipeManager.CreateBatchRandomRecipes(amountToCreate, null);
+        if (!createBatchRecipeResult.IsSuccess)
+        {
+            _logger.LogError($"DatabaseJob failed to create batch recipe: {createBatchRecipeResult.ErrorMessage}");
+            return;
+        }
         
         _logger.LogInformation($"DatabaseJob executed at {DateTime.Now}");
     }
