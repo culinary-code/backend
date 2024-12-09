6826c404abd5736f5e0a8f001b5f4499076316a8
HannesC <hannes.coenen@infosupport.com>
2024-12-09 07:51:02 +0100

    grocerycontroller changed

--- WEBAPI/Controllers/GroceryController.cs
+++ WEBAPI/Controllers/GroceryController.cs
@@ -8,6 +8,7 @@ using DOM.Exceptions;
 using DOM.MealPlanning;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using WEBAPI.ResultExtension;
 
 namespace WEBAPI.Controllers;
 
@@ -20,7 +21,8 @@ public class GroceryController : ControllerBase
     private readonly IIdentityProviderService _identityProviderService;
     private readonly ILogger<GroceryController> _logger;
 
-    public GroceryController(IGroceryManager groceryManager, IIdentityProviderService identityProviderService, ILogger<GroceryController> logger)
+    public GroceryController(IGroceryManager groceryManager, IIdentityProviderService identityProviderService,
+        ILogger<GroceryController> logger)
     {
         _groceryManager = groceryManager;
         _identityProviderService = identityProviderService;
@@ -30,71 +32,65 @@ public class GroceryController : ControllerBase
     [HttpGet("grocery-list")]
     public async Task<IActionResult> GetGroceryListById()
     {
-        try
+        string token = Request.Headers["Authorization"].ToString().Substring(7);
+        var userIdResult = _identityProviderService.GetGuidFromAccessToken(token);
+        if (!userIdResult.IsSuccess)
         {
-            Guid userId = _identityProviderService.GetGuidFromAccessToken(Request.Headers["Authorization"].ToString().Substring(7));
-            var groceryList = await _groceryManager.GetGroceryListWithNextWeek(userId);
-            return Ok(groceryList);
+            return BadRequest(userIdResult.ErrorMessage);
         }
-        catch (GroceryListNotFoundException e)
-        {
-            _logger.LogError("An error occured trying to fetch user: {ErrorMessage}", e.Message);
-            return NotFound(e.Message);
-        }
+
+        var userId = userIdResult.Value;
+        var groceryList = await _groceryManager.GetGroceryListWithNextWeek(userId);
+        return groceryList.ToActionResult();
     }
-    
+
     [HttpGet("account/grocery-list")]
     public async Task<IActionResult> GetGroceryListByAccessToken()
     {
-        try
+        string token = Request.Headers["Authorization"].ToString().Substring(7);
+        var userIdResult = _identityProviderService.GetGuidFromAccessToken(token);
+        if (!userIdResult.IsSuccess)
         {
-            Guid userId = _identityProviderService.GetGuidFromAccessToken(Request.Headers["Authorization"].ToString().Substring(7));
-            var groceryListDto = await _groceryManager.GetGroceryListByAccountId(userId.ToString());
-            return Ok(groceryListDto);
+            return BadRequest(userIdResult.ErrorMessage);
         }
-        catch (JwtTokenException e)
-        {
-            _logger.LogError("An error occurred while trying to get user: {ErrorMessage}", e.Message);
-            return Unauthorized("Invalid access token: " + e.Message);
-        }
-        catch (GroceryListNotFoundException e)
-        {
-            return NotFound(e.Message);
-        }
+
+        var userId = userIdResult.Value;
+        var groceryListDto = await _groceryManager.GetGroceryListByAccountId(userId.ToString());
+        return groceryListDto.ToActionResult();
     }
 
     [HttpPut("grocery-list/add-item")]
     public async Task<IActionResult> AddItemToGroceryList([FromBody] ItemQuantityDto newItem)
     {
-        Guid userId = _identityProviderService.GetGuidFromAccessToken(Request.Headers["Authorization"].ToString().Substring(7));
+        string token = Request.Headers["Authorization"].ToString().Substring(7);
+        var userIdResult = _identityProviderService.GetGuidFromAccessToken(token);
+        if (!userIdResult.IsSuccess)
+        {
+            return BadRequest(userIdResult.ErrorMessage);
+        }
 
-        try
-        {
-            await _groceryManager.AddItemToGroceryList(userId, newItem);
-            _logger.LogInformation("Item added to grocery list.");
-            return Ok($"{newItem} added to grocery list of account: {userId}.");
-        }
-        catch (GroceryListNotFoundException ex)
-        {
-            _logger.LogError($"Grocery list was not found for account with id: {userId}");
-            return BadRequest(ex.Message);
-        }
+        var userId = userIdResult.Value;
+
+        var addItemToGroceryListResult = await _groceryManager.AddItemToGroceryList(userId, newItem);
+        _logger.LogInformation("Item added to grocery list.");
+        return addItemToGroceryListResult.ToActionResult();
     }
-    
+
     [HttpDelete("grocery-list/items")]
     public async Task<IActionResult> DeleteItemFromList([FromBody] ItemQuantityDto removeItem)
     {
-        Guid userId = _identityProviderService.GetGuidFromAccessToken(Request.Headers["Authorization"].ToString().Substring(7));
-        try
+        string token = Request.Headers["Authorization"].ToString().Substring(7);
+        var userIdResult = _identityProviderService.GetGuidFromAccessToken(token);
+        if (!userIdResult.IsSuccess)
         {
-            await _groceryManager.RemoveItemFromGroceryList(userId, removeItem);
-            _logger.LogInformation("Item {ItemQuantityId} deleted from grocery list of account {UserId}.", removeItem.ItemQuantityId, userId);
-            return Ok(new { message = "Item deleted successfully." });
+            return BadRequest(userIdResult.ErrorMessage);
         }
-        catch (GroceryListNotFoundException)
-        {
-            _logger.LogWarning("Grocery list of account {UserId} or item {ItemQuantityId} not found.", userId, removeItem.ItemQuantityId);
-            return NotFound(new { message = "Grocery list or item not found." });
-        }
+
+        var userId = userIdResult.Value;
+
+        var removeItemFromGroceryListResult = await _groceryManager.RemoveItemFromGroceryList(userId, removeItem);
+        _logger.LogInformation("Item {ItemQuantityId} deleted from grocery list of account {UserId}.",
+            removeItem.ItemQuantityId, userId);
+        return removeItemFromGroceryListResult.ToActionResult();
     }
 }
